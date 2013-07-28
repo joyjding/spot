cb723ec8bf2c5fe6e43da2e8362bb0e78e71aa06
joyjding <joy.j.ding@gmail.com>
2013-07-27 17:18:48 -0700

    taking simple token objects from tokens array, checking them against the symbol table, making them into new token objects that the parser can parse.

--- example.pratt.js
+++ example.pratt.js
@@ -2,6 +2,7 @@
 //Source: http://javascript.crockford.com/tdop/tdop.html
 
 
+///////////////////SYMBOLS////////////////////
 
 //Every token, such as an operator or identifier, will inherit from a symbol.
 //All symbols (which will determine the types of tokens) will be kept in a symbol_table object
@@ -56,6 +57,68 @@ symbol("}");
 symbol("else");
 symbol("(end)"); //indicates end of token stream.
 symbol("(name)"); //prototype for new names, such as variable names.
+
+///////////////////TOKENS////////////////////
+
+//Assuming that the source text has been transformed into an array of simple token objects(tokens),
+//each containing a type member("name", "string", "number", or "operator")
+
+//the token variable always contains the current token LOOK GLOBAL VARIABLE
+
+var token;
+
+//The advance function makes a new token object from the next simple token in the array,
+//and assigns it to the token variable.
+//It can take an optional id parameter which it can check against the id of the prev toke. WHY would it check?
+//The new token's arity is "name", "literal", or "operator".
+//Its arity may be changed later to "binary", "unary", or "statement" when we know more about its role in the program.
+
+var advance = function (id) //set the variable name advance to the value of a function that optionally takes id
+//Are all parameters optional? How does this function know that the param is optional?
+{
+	var a, o, t, v;
+	//check that there is no id error
+	if (id && token.id !== id) //If both id and token.id do not equal id WEIRD
+	{
+		token.error("Expected '" + id + "'.")
+	}
+	//check to see if token stream has ended
+	if (token_nr >= tokens.length) //what is token_nr? I guess if num tokens>=length of tokens array
+	{
+		token = symbol_table["(end)"];
+		return; //what is being returned here?
+	}
+	//now we get to the good part.
+	t = tokens[token_nr]; //Set t = to the token at tokens[token_nr];
+	token_nr +=1 //advance token_nr Why isn't token_nr declared earlier?
+	v = t.value;
+	a = t.type;
+	if (a === "name") //makes sense to use === here
+	{
+		o = scope.find(v); //no idea what just happened. Set o to some object??
+	}
+	else if (a === "operator") //if token type is operator
+	{
+		o = symbol_table[v] // Use the token's value as the key to look in symbol_table; set o to be this object
+		if (!o) 
+		{
+			t.error("Unknown operator."); //if the object with key [value] does not exist, raise error. Why is this t.error? 
+		}
+	}
+	else if (a === "string" || a === "number") //previously a = t.type, now we're changing the type
+	{
+		a = "literal"; //save "literal" in a, later we set a bunch of things in the token
+		o = symbol_table["(literal)"]; // set o to be the object saved in symbol_table["(literal)"]
+	}
+	else {
+		t.error("Unexpected token."); //if the type of the token is neither a name, operator, string, or number, throw an error
+	}
+	token = Object.create(o); //create a new object from o. Set global token to that object. 
+	//Whoa, is Object.create just an inherent js method?
+	token.value = v;
+	token.arity = a;
+	return token;
+};
 
 
 
