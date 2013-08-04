9ab4c9933c2ecff04050202b81629857f3c1a369
joyjding <joy.j.ding@gmail.com>
2013-08-04 14:41:02 -0700

    added SymbolBase class, symbol function, created basic symbols using symbol function, changed lexer categories to base categories, changed tokenize to filter for lexer categories and do symbol_dict lookups

--- spot.py
+++ spot.py
@@ -8,23 +8,19 @@ import ply.lex as lex #import ply library
 
 # List of token names
 token_names = [
-	'INT',
-	'ADD_OP',
-	'SUB_OP',
-	'MUL_OP',
-	'DIV_OP',
-	'POW_OP'
+	'NUMBER',
+	'STRING',
+	'OPERATOR',
+	'NAME'
 ]
 
 tokens = token_names
 
 #-Token Functions----------------------------- 
-t_INT = r'[0-9][0-9]*'
-t_ADD_OP = r'\+'
-t_SUB_OP = r'-'
-t_MUL_OP = r'\*'
-t_DIV_OP = r'/'
-t_POW_OP = r'\*\*'
+t_NUMBER = r'[0-9][0-9]*'
+t_STRING = r'"[A-Za-z0-9_]*"'
+t_OPERATOR = r'[\+-/\*]'
+t_NAME = r'[A-Za-z_][A-Za0-9_]*'
 
 # Defines a rule for tracking line numbers
 def t_newline(t):
@@ -59,87 +55,6 @@ print lex_tokens
 # PARSER
 # Top Down Precedence Parsing
 ################################################################################
-
-
-
-# #-the Symbol class and the make_symbol function----------------------------------
-
-# # Every token will inherit from a symbol, all of which are held in the symbol_dict
-# symbol_dict = {}
-
-# # Now I'm going to create an original_symbol object, which is the prototype for all of the symbols
-# class Symbol:
-# 	""" In Crockford, he implements an original_symbol class, and then adds
-# 	methods to it in a symbol function object but I think it might be more
-# 	elegant to simply create one symbol class that already has id and lbp methods. """
-# 	def __init__(self, id, bp):
-# 		self.id = id
-# 		self.lbp = bp
-# 		self.value = id #-------I still don't really understand why id and value are both id. It's probably changed later
-# 	def nud(self): 
-# 		return None
-# 	def led (self):
-# 		return None
-
-# # Now I'm going to write a function that makes symbols
-
-# def make_symbol(id, bp=0):
-# 	symbol = symbol_dict.get(id)
-# 	if symbol: #if id does not exist in symbol_dict
-# 		if bp>=symbol.lbp: #-------why would I want to update this?
-# 			symbol.lbp = bp
-# 	else:
-# 		symbol = Symbol(id, bp) # make a new instance of Symbol
-# 		symbol_dict[id] = symbol # put the new symbol into the dictionary
-# 	return symbol # return the symbol object
-
-# # Now to make a couple of popular separator and closer symbols
-
-# make_symbol(",")
-# make_symbol(".")
-# make_symbol(":")
-# make_symbol(";")
-# make_symbol(")")
-# make_symbol("]")
-# make_symbol("}")
-# # make_symbol("else") --- commented out b/c I don't really understand why it's here
-# make_symbol("(end)")
-# make_symbol("(name)")
-
-# #-Tokens--------------------------------------------------------------------------------
-
-# #Global variable token
-# TOKEN = None
-
-# #The advance function takes the next LexToken, transforms it, assigns it to the token variable
-# def advance_token (id = None): #optional id argument
-# 	if id and (TOKEN.id != id):
-# 		print "Did not get expected token" # -----Later on, this will raise an error
-# 	if TOKEN[token_num] > len(lex_tokens): #-----Do people usually destroy tokens?
-# 		TOKEN = symbol_dict["(end)"]
-	
-# 	newtoken = lex_tokens[TOKEN[token_num]] #----Do I start it off with token_num = 0?
-# 	token_num +=1 #----increment token_num
-# 	value = newtoken[value]
-# 	arity = newtoken[type]
-
-# 	if arity == "name": #----change lextokens category
-# 		#more code here
-# 		print "this token is a name token"
-# 	elif arity === "operator":
-# 		#more code here
-# 		print "this token is an operator"
-# 	elif arity === "string" or arity === "number":
-# 		arity = "literal"
-# 		object_prototype = symbol_dict["(literal)"]
-# 	else:
-# 		print "Unexpected token."
-
-# 	TOKEN = Symbol(object_prototype)
-# 	TOKEN.value = value
-# 	TOKEN.arity = arity
-
-# 	return TOKEN
 
 def expression(rbp=0):
     global token
@@ -216,23 +131,63 @@ class OperatorPowToken:
 class EndToken:
     lbp = 0
 
+
+class SymbolBase:
+	id = None
+	value = None
+	first = None
+	second = None
+	third = None
+
+	def nud(self):
+		raise SyntaxError("no nud")
+	def led(self, left):
+		raise SyntaxError("no led")
+	def __repr__(self):
+		if self.id == "(name)" or self.id == "(literal)":
+			return "(%s %s)" % (self.id[1:-1], self.value) #cuts parens off self.id
+		out = [self.id, self.first, self.second, self.third]
+		out = map(str, filter(None, out))
+		return "(" + " ".join(out) + ")"
+
+symbol_dict = {}
+
+def symbol(id, bp=0):
+	try: 
+		s = symbol_dict[id]
+	except KeyError:
+		class s(SymbolBase):
+			pass
+		s.__name__ = "symbol:" + id #for debugging
+		s.id = id
+		s.lbp = bp
+		symbol_dict[id] = s
+	else: 
+		s.lbp = max(bp, s.lbp)
+	return s
+
+symbol("(literal)")
+symbol("+", 10); symbol("-", 10)
+symbol("*", 20); symbol("/", 20)
+symbol("(end)") 
+
+
 def tokenize(program):
     for lex_token in lex_tokens:
-    	if lex_token['id']=='INT':
-    		yield LiteralToken(lex_token['value'])
-    	elif lex_token['id']=='ADD_OP':
-    		yield OperatorAddToken()
-    	elif lex_token['id']=='SUB_OP':
-    		yield OperatorSubToken()
-    	elif lex_token['id']=='MUL_OP':
-    		yield OperatorMulToken()
-    	elif lex_token['id']=='DIV_OP':
-    		yield OperatorDivToken()
-    	elif lex_token['id']=='POW_OP':
-    		yield OperatorPowToken()
-    	else: 
-    		raise SyntaxError("unknown operator")
-    yield EndToken()
+    	if (lex_token['id'] == 'NUMBER' or lex_token['id'] == 'STRING'):
+    		symbol_class = symbol_dict["(literal)"]
+    		new_symbol = symbol_class()
+    		new_symbol.value = lex_token['value']
+    		yield new_symbol
+    	elif lex_token['id'] == 'OPERATOR':
+    		symbol_class = symbol_dict.get(lex_token['id'])
+    		if not symbol_class:
+    			raise SyntaxError("Unknown operator")
+    		yield symbol_class() #make a new instance of the operator prototype class. No need to set a value b/c operators don't have values.
+    	elif lex_token['id'] == 'NAME':
+    		pass
+    new_symbol = symbol_dict["(end)"]
+    yield new_symbol()
 
 def parse(program):
     global token, next
